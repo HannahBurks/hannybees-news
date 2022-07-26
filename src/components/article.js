a9450aeb375bedbe201070c1ff20400d2fab0fec
HannahBurks <hannahburks@hotmail.com>
2022-07-26 15:42:07 +0100

    adding avatar to individual articles

--- src/components/article.js
+++ src/components/article.js
@@ -10,6 +10,22 @@ const [articleData, setArticleData] = useState([])
 const[commentData, setCommentData] = useState([])
 const [isLoading, setIsLoading] = useState(true)
 
+
+function getAvatarUrl(user){
+    const userRef = {
+      tickle122:'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953',
+      grumpy19:'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013',
+      happyamy2016: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+      cooljmessy: 'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
+      weegembump:'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553',
+      jessjelly: 'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
+    }
+    let result = userRef[user]
+   return result
+  }
+
+
+
     useEffect(() => {
         fetch(`https://hannybees-news-app.herokuapp.com/api/articles${articleIdRoute}`).then((response) => {
             return response.json();
@@ -40,6 +56,7 @@ const [isLoading, setIsLoading] = useState(true)
 <div className='Individual_article_container'>
     <div className='Article_container'>
 <h2>{articleData.title}</h2>
+<img className = 'allArticles_avatar'src={getAvatarUrl(articleData.author)}/>
 <h3> Author: {articleData.author}</h3>
 <h2> Article Votes:{articleData.votes}</h2>
 <p>{articleData.body}</p>
