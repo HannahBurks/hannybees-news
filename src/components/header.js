7564c158dccb5687d38b6dea1ea20a6f3c720065
HannahBurks <hannahburks@hotmail.com>
2022-08-01 12:27:04 +0100

    amending border

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ export default function Header(){
     
 
 return (
-    <div className='header-cotainer'>
+    <div className='header-container'>
 <div className = 'image-box'>
 <img src={ require('../images/hannybee-news-logo.jpg')} alt="Hannybees news logo of a bee" width='110px' className='logo'/>
  </div>
